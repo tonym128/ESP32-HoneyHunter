70003cd0140b8e6464f14511c58230190ed50c29
Tony Mamacos <tmamacos@gmail.com>
2020-01-25 00:54:18 +0200

    Added blank secrets back in

---
+++ secrets.h
@@ -0,0 +1,7 @@
+#ifndef _SECRETS
+#define _SECRETS
+static char *host = (char*)"esp32";
+static char *ssid = (char*)"ssid";			 // To avoid having to enter it manually
+static char *password = (char*)"password"; // To avoid having to enter it manually
+
+#endif
